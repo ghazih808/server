664d51e6d479b6a108db096c61336c676479f14d
ghazih808 <116555180+ghazih808@users.noreply.github.com>
2022-12-08 00:00:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const e=express();
+e.get('/',(req,res)=>
+{
+    res.send("<class = 'grocery' ul><li>Coffee</li><li>Tea</li><li>Milk</li></ul>")
+})
+e.get('/Bilal',(req,res)=>
+{
+    res.send("hello bilal")
+})
+e.listen(process.env.PORT||300,()=>{console.log('listening express server')})
